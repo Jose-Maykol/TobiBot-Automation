68240c7a2b728fec1e5bd76acd8a5007c39bf8d7
José Maykol <jpaniura@unsa.edu.pe>
2023-09-02 22:04:02 -0500

    feat: get torrent search info

--- index.js
+++ index.js
@@ -1,58 +1,17 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 import { getAnimeToDownload } from './controllers/anime.controller.js';
+import animeScapper from './scrapper.js';
 
 const animeToDownload = await getAnimeToDownload();
-const animePageUrl = 'https://nyaa.si/';
-let animeTextSearch = 'Came Esp One Piece 1080 1072';
-const torrent = {
-  name: null,
-  linkDownload: null,
-  size: null,
-  date: null
-};
 
 if (animeToDownload.length === 0) {
   console.log('No hay anime por descargar')
 } else {
-  animeToDownload.forEach((anime) =>{
+  animeToDownload.forEach( async (anime) =>{
+    const animeTextSearch = `${anime.subs_name} ${anime.anime_name} 1080 ${anime.chapter.toString().padStart(2, '0')}`
     console.log('Anime: ', anime)
+    console.log('Texto a buscar', animeTextSearch);
+    const torrentDownload = await animeScapper(animeTextSearch)
+    console.log(torrentDownload);
   });
 }
-
-(async () => {
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto(animePageUrl);
-  // Busqueda de torrents
-  await page.type('.search-bar', animeTextSearch);
-  await page.waitForXPath('//*[@id="navbar"]/form/div/div[3]/button');
-  const [ buttonSearch ] = await page.$x('//*[@id="navbar"]/form/div/div[3]/button');
-  await buttonSearch.click();
-
-  await page.waitForXPath('/html/body/div/div[1]/table');
-  const tableSearch = await page.$x('/html/body/div/div[1]/table');
-  const rowsSearch = await tableSearch[0].$$('tr');
-
-  if (rowsSearch.length > 0) {
-    await Promise.all(rowsSearch.slice(1).map(async (row) => {
-      const columns = await row.$$('td');
-      const torrentFilename = await columns[1].evaluate(node => node.textContent);
-      const torrentLinks = await columns[2].$$('a');
-      const firstTorrentLink = await torrentLinks[0].getProperty('href')
-      const torrentLinkDownload = await firstTorrentLink.jsonValue();
-      const torrentSize = await columns[3].evaluate(node => node.textContent);
-      const torrentDate = await columns[4].evaluate(node => node.textContent);
-
-      torrent.name = torrentFilename.replace(/\t|\n/g, '')
-      torrent.linkDownload = torrentLinkDownload
-      torrent.size = torrentSize
-      torrent.date = torrentDate
-
-    }));
-  } else {
-    console.log('No se encontraron resultados');
-  } 
-  console.log(torrent);
-  //await browser.close();
-})();
---
+++ scrapper.js
@@ -0,0 +1,66 @@
+import puppeteer from 'puppeteer';
+
+async function animeScapper(textSearch) {
+
+  const animePageUrl = 'https://nyaa.si/';
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
+  const page = await browser.newPage();
+  let torrent = {
+    name: null,    
+    linkDownload: null,
+    size: null,
+    date: null
+  };
+
+  try {
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(animePageUrl);
+    await page.type('.search-bar', textSearch);
+    await page.waitForXPath('//*[@id="navbar"]/form/div/div[3]/button');
+    const [ buttonSearch ] = await page.$x('//*[@id="navbar"]/form/div/div[3]/button');
+    await buttonSearch.click();
+
+    await page.waitForXPath('/html/body/div/div[1]/table');
+    const tableSearch = await page.$x('/html/body/div/div[1]/table');
+    const rowsSearch = await tableSearch[0].$$('tr');
+
+    if (rowsSearch.length > 0) {
+      console.log('Se encontraron resultados');
+      await Promise.all(rowsSearch.slice(1).map(async (row) => {
+        const columns = await row.$$('td');
+        const torrentFilename = await columns[1].evaluate(node => node.textContent);
+        const torrentLinks = await columns[2].$$('a');
+        const firstTorrentLink = await torrentLinks[0].getProperty('href')
+        const torrentLinkDownload = await firstTorrentLink.jsonValue();
+        const torrentSize = await columns[3].evaluate(node => node.textContent);
+        const torrentDate = await columns[4].evaluate(node => node.textContent);
+        
+        /* const torrent = {
+          name: torrentFilename.replace(/\t|\n/g, ''),
+          linkDownload: torrentLinkDownload,
+          size: torrentSize,
+          date: torrentDate
+        }; */
+
+        torrent.name = torrentFilename.replace(/\t|\n/g, '')
+        torrent.linkDownload = torrentLinkDownload
+        torrent.size = torrentSize
+        torrent.date = torrentDate
+
+        console.log(torrent);
+      }));
+    } else {
+      console.log('No se encontraron resultados');
+    }
+
+    return torrent;
+    
+  } catch (error) {
+    console.error('Error en el scraper:', error);
+    throw error;
+  }/*  finally {
+    await browser.close();
+  } */
+}
+
+export default animeScapper;
